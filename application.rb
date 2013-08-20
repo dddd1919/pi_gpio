48e69f0843dccc4d067ce66e46c827a124023157
dddd1919 <407969178@qq.com>
2013-08-20 18:22:35 +0800

    添加待定接口，gpio配置文件

--- application.rb
+++ application.rb
@@ -1,7 +1,7 @@
 require "sinatra"
 require "erb"
 # require "json"
-ON_OFF_SET = ["3.0V", "0.0V"]
+ON_OFF_SET = ["低电位", "高电位"]
 
 set :views, ['views']
 
@@ -10,7 +10,7 @@ get "/" do
 end
 
 get "/gpio" do
-  ## 每次请求页面要把端口状态重置
+  ## 初始化针脚，读取状态后发出信号
   erb :gpio
 end
 
@@ -18,8 +18,17 @@ get "/about" do
   erb :about
 end
 
-post "/test" do
+post "/set_pin" do
   set = params["onoff"]
+  ## TODO 设置电位
   retval_index = 1 - ON_OFF_SET.index(set)
   ON_OFF_SET[retval_index]
+end
+
+get '/get_pin' do
+  ## TODO 一次性获取所有针脚状态
+end
+
+post '/reset_pin' do
+  ## TODO 重置所有针脚状态为输出低电位
 end
