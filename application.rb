ba3b03ca023d78c3d64a20f32014ada7a6ca9c13
dddd1919 <407969178@qq.com>
2013-09-05 18:50:34 +0800

    修改faye和前端样式

--- application.rb
+++ application.rb
@@ -9,6 +9,7 @@ require 'lib/pi_gpio.rb'
 require "json"
 SWITCH = ["低电位","高电位"]
 SWITCH_DATA = [:off, :on]
+PIN_WATCH_THREAD = {}
 PIN_INFO = YAML::load(File.open("lib/gpio_info.yml"))
 PIN = Pi_gpio::Gpio.new(PIN_INFO)
 
@@ -18,7 +19,7 @@ get "/" do
   erb :index
 end
 
-## 初始进入控制器直接获取针脚状态
+## Init pin info from config file and Raspberry
 get "/gpio" do
   # PIN_INFO.each { |num, pi| PIN_INFO[num] = pi.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}}
   # @pin_status = PIN_INFO
@@ -30,7 +31,7 @@ get "/about" do
   erb :about
 end
 
-## 这部分接收针脚变化的ajax请求
+## receive Pin ajax request from client
 post "/set_pin" do
   port = params[:id]
   status = params[:onoff]
@@ -42,14 +43,42 @@ post "/set_pin" do
   return result
 end
 
-## 重置所有接口
+## reset all Pin
 post "/reset_pin" do
   PIN.replace_port
   redirect "/gpio"
 end
 
-##TODO 端口变为输入时使用长轮询实时刷新前端状态
-# params = {}
-# uri = URI.parse("http://localhost:3000/faye")
-# params["message"] = {"channel" => "/messages/new", "data" => "hello"}.to_json
-# result = Net::HTTP.post_form(uri, params)
+post "/inout_switch" do
+  retval = false
+  if params[:direction].to_s == "in"
+    retval = watch_pin(params[:pin])
+  else
+    retval = unwatch_pin(params[:pin])
+  end
+  return retval.to_s
+end
+
+def watch_pin(pin)
+  retval  = false
+  new_watch_thread = watch :pin => pin do
+    ## post pin changing message to faye server
+    params = {}
+    uri = URI.parse("http://localhost:3000/faye")
+    params["message"] = {"channel" => "/messages/new", "data" => {"pin" => pin, "switch" => SWITCH[value]}}.to_json
+    response = Net::HTTP.post_form(uri, params)
+    retval = JSON.parse(response.body)[0]["successful"]
+  end
+  ## save thread message to PIN_WATCH_THREAD
+  PIN_WATCH_THREAD[pin] = new_watch_thread
+  return true
+end
+
+def unwatch_pin(pin)
+  retval = false
+  unless PIN_WATCH_THREAD[pin].nil?
+    PIN_WATCH_THREAD[pin].kill ## kill watch thread
+    retval = !PIN_WATCH_THREAD.delete(pin).nil?
+  end
+  return retval
+end
--- public/js/webiopi.js
+++ public/js/webiopi.js
@@ -3,7 +3,7 @@ $(".btn-onoff").click(function(){
   if (origin_class.match("btn-onoff"))
   {
 	  var $this = $(this);
-	  $.post("set_pin", { onoff: $this.html(), id: $this.parent('div').attr("id")}, function(data,status){
+	  $.post("set_pin", { onoff: $this.html(), id: $this.attr("id")}, function(data,status){
 	    if ($this.html() == data)  //如果设置失败需要把按钮状态还原
 	    {
 	      $this.attr("class", origin_class)
@@ -11,28 +11,40 @@ $(".btn-onoff").click(function(){
 	    else
 	    {
 	      $this.html(data);
-	    }          
+	    }
 	  });
 	};
 });
 
 $(document).ready(function(){
+	$(".switch-left").text("IN");
+  $(".switch-right").text("OUT");
 	$('div[class="switch has-switch"]').click(function(){
-		var class_name = $(this).parent().attr("controller");
-		var button_status = $(this).children().attr('class');
-		var pin_button = $(this).parent('td').siblings('td[class="' + class_name + '"]')
+		var $this = $(this)
+		var class_name = $this.parent().attr("controller");
+		var button_status = $this.children().attr('class');
+		var pin_button = $this.parent('td').siblings('td[class="' + class_name + '"]');
+		var pin_info = $this.parent('td').siblings('td[info="' + class_name + '"]').children('span').text();
 		if (button_status.match('switch-on')){
-			pin_button.children("div").removeAttr("data-toggle");
-			pin_button.children("div").children('button').attr('class', 'btn btn-mini btn-success');
-			pin_button.children("div").children('button').text('低电位');
-			console.log(pin_button);
-		}
-		else{
-			pin_button.children("div").attr("data-toggle", "buttons-checkbox");
-			pin_button.children("div").children('button').attr('class', 'btn btn-mini btn-success btn-onoff');
-			pin_button.children("div").children('button').text('低电位');
-			console.log(pin_button);
-		}		
+			$.post("inout_switch", { pin: pin_info, direction:"in"}, function(data,status){
+		    if (status == "success" && data == "true"){  //如果设置失败需要把按钮状态还原
+			    pin_button.children("button").removeAttr("data-toggle");
+					pin_button.children("button").attr('class', 'btn btn-success');
+					pin_button.children("button").text('低电位');
+			  } else {
+			  	$this.children().bootstrapSwitch('toggleState');
+			  };
+			});
+		}	else{
+			$.post("inout_switch", { pin: pin_info, direction:"out"}, function(data,status){
+		    if (status == "success" && data == "true"){  //如果设置失败需要把按钮状态还原
+			    pin_button.children("button").attr("data-toggle", "button");
+					pin_button.children("button").attr('class', 'btn btn-success btn-onoff');
+					pin_button.children("button").text('低电位');
+			  } else {
+			  	$this.children().bootstrapSwitch('toggleState');
+			  };
+			});
+		};
 	});
-
 });
